8df96b6cb3a21693ad0a43aacde204840e8ff70c
Tarthan <61007686+Tarthan@users.noreply.github.com>
2020-02-13 20:43:45 +0800

    Testing- map
    Made map

--- Testing/Assets/Scripts/PlayerController.cs
+++ Testing/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float turnSpeed = 100f;
-
+    public float moveSpeed = 5f;     //Distance moved (units per second) when user holds the up or down arrow
+    public float turnSpeed = 100f;   //Rotaing speed (degrees per second) when user holds the left or right arrow
+    public float jumpHeight = 5f;       // Upward velocity when user presses spacebar.
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +16,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Forwards and backwards
+
         if (Input.GetKey(KeyCode.UpArrow) == true)
         {
             this.transform.position += this.transform.forward * Time.deltaTime * this.moveSpeed;
@@ -24,15 +26,24 @@ public class PlayerController : MonoBehaviour
         {
             this.transform.position -= this.transform.forward * Time.deltaTime * this.moveSpeed;
         }
+        // rotating left and right
 
         if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
-            this.transform.Rotate -= (this.transform.right, Time.deltaTime * -this.turnSpeed);
+            this.transform.Rotate(this.transform.up, Time.deltaTime * -this.turnSpeed);
         }
-
         if (Input.GetKey(KeyCode.RightArrow) == true)
         {
             this.transform.Rotate(this.transform.up, Time.deltaTime * this.turnSpeed);
         }
+        // spacebar to jump.
+
+        if (Input.GetKey(KeyCode.Space) == true && Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 0.01f)
+        {
+            this.GetComponent<Rigidbody>().velocity += Vector3.up * this.jumpHeight;
+        }
+
+
+
     }
 }
